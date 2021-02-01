90ff24925f8bf28b58c966cce976716b6b01ffdc
AnKiWoong <ankiwoong@gmail.com>
2021-02-02 07:20:33 +0900

    문자열

--- Ch5/address.c
+++ Ch5/address.c
@@ -1,8 +1,22 @@
+// #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 50;
+    // int n = 50;
     // printf("%p\n", &n);
-    printf("%i\n", *&n);
+    // printf("%i\n", *&n);
+
+    // int *p = &n;
+    // printf("%p\n", p);
+    // printf("%i\n", *p);
+
+    // string s = "EMMA";
+    char *s = "EMMA";
+    // printf("%s\n", s);
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
 }
