ae9edec08d2fe1dfd5e626ef11212be1483eb23a
AnKiWoong <ankiwoong@gmail.com>
2021-02-01 12:22:46 +0900

    포인터

---
+++ Ch5/point_type.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("The size of an int pointer is %ld bytes!\n", sizeof(char *));
+    printf("The size of a char pointer is %ld bytes!\n", sizeof(int *));
+    printf("The size of a short pointer is %ld bytes!\n", sizeof(short *));
+    printf("The size of a long pointer is %ld bytes!\n", sizeof(long *));
+    printf("The size of a float pointer is %ld bytes!\n", sizeof(float *));
+    printf("The size of a double pointer is %ld bytes!\n", sizeof(double *));
+    printf("The size of a void pointer is %ld bytes!\n", sizeof(void *));
+    return (0);
+}
